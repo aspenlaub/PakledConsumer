80fda7537f420ffb3635867eded61980d27248d3
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-10-12 12:39:14 +0200

    PakledCore update

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Aspenlaub.Net.GitHub.CSharp.PakledCore;
+using Autofac;
 
 namespace Aspenlaub.Net.GitHub.CSharp.PakledConsumerCore {
     internal class Program {
@@ -7,7 +8,8 @@ namespace Aspenlaub.Net.GitHub.CSharp.PakledConsumerCore {
             Console.WriteLine("A small story from the star trek universe:");
             var thing = new Thing {State = ThingState.Broken};
             Console.WriteLine($"Grebnedlog's log, star date 42723.8: we have a {thing.GetType().Name} that is {Enum.GetName(typeof(ThingState), thing.State)}");
-            var goMaker = new GoMaker();
+            var container = new ContainerBuilder().UsePakledCore().Build();
+            var goMaker = container.Resolve<IGoMaker>();
             Console.WriteLine($"Asking the {goMaker.GetType().Name} to make it go");
             goMaker.MakeItGo(thing);
             Console.WriteLine($"The {thing.GetType().Name} is now {Enum.GetName(typeof(ThingState), thing.State)}");
